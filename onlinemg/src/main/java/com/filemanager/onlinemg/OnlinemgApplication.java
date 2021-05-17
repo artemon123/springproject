273361d3459599c7e23518d43c0946a19a257170
artemon123 <artemlach8@gmail.com>
2021-05-17 20:09:10 +0300

    spring boot core web vs tools

---
+++ onlinemg/src/main/java/com/filemanager/onlinemg/OnlinemgApplication.java
@@ -0,0 +1,13 @@
+package com.filemanager.onlinemg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinemgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinemgApplication.class, args);
+	}
+
+}
---
+++ onlinemg/src/test/java/com/filemanager/onlinemg/OnlinemgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filemanager.onlinemg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinemgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
